d0c6fbe7a586bf91bbc9c48628fe2032c83b51c8
Timzz <suu_irt@163.com>
2026-03-18 14:01:46 +0800

    完善了GMP计算

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,28 @@
-#include "myheader.h"
-
-const unsigned int N = 10000;
-
-typedef number<cpp_dec_float<N>> myfloat;
+#include "picalg.h"
 
 int main() {
-    myfloat pic = boost::math::constants::pi<myfloat>();
-    std::cout << "Pi to " << N << " decimal places:" << std::endl;
-    std::cout << pic.str(N) << std::endl;
+    long digits = 1000; // Change this to 10000 or more!
+    
+    // 1 iteration gives ~14.18 digits
+    long N = digits / 14 + 1;
+    unsigned long prec_bits = digits * 3.321928 + 64; 
+    mpf_set_default_prec(prec_bits);
+
+    std::cout << "Calculating " << digits << " digits of Pi..." << std::endl;
+
+    // 1. Run Binary Splitting to get T and Q
+    PQT res = compute_PQT(0, N);
+
+    // 2. Final calculation: Pi = (D * sqrt(E) * Q) / (A * Q + T)
+    mpf_class pi;
+    mpf_class q_f(res.Q);
+    mpf_class t_f(res.T);
+    
+    pi = D * sqrt((mpf_class)E) * q_f;
+    pi /= (A * q_f + t_f);
+
+    std::cout.precision(digits);
+    std::cout << pi << std::endl;
+
     return 0;
 }
--- src/myheader.h
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-#include <boost/multiprecision/cpp_dec_float.hpp>
-#include <boost/math/constants/constants.hpp>
-
-using namespace boost::multiprecision;
