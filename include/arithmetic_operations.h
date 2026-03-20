0bd08a17c8da68c4507dae69902ee9b47ef83d9f
Timzz <suu_irt@163.com>
2026-03-20 15:19:22 +0800

    feat: 完成了算数函数基本结构

--- include/arithmetic_operations.h
+++ include/arithmetic_operations.h
@@ -5,6 +5,15 @@
 #include <gmpxx.h>
 
 class ArithmeticOperations {
+public:
+    static const mpf_class add(const mpf_class& x, const mpf_class& y);
+    static const mpf_class subtract(const mpf_class& x, const mpf_class& y);
+    static const mpf_class multiply(const mpf_class& x, const mpf_class& y);
+    static const mpf_class divide(const mpf_class& x, const mpf_class& y);
+    static const mpf_class power(const mpf_class& x, const mpf_class& y);
+
+    static void SetDefaultPresition(size_t n);
+private:
 
 };
 
