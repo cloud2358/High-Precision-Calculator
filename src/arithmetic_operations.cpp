e55f83b4e5bcfc5f558a5f4b6d326c1aa8a8c7a8
Timzz <suu_irt@163.com>
2026-03-24 20:25:58 +0800

    feat: 搭建了arithmetic_operations.cpp中的基本结构

---
+++ src/arithmetic_operations.cpp
@@ -0,0 +1,21 @@
+#include <arithmetic_operations.h>
+
+static const mpf_class add(const mpf_class &x, const mpf_class &y) {
+    return x + y;
+}
+
+static const mpf_class subtract(const mpf_class &x, const mpf_class &y) {
+    return x - y;
+}
+
+static const mpf_class multiply(const mpf_class &x, const mpf_class &y) {
+    return x * y;
+}
+
+static const mpf_class divide(const mpf_class &x, const mpf_class &y) {
+    return x / y;
+}
+
+static const mpf_class power(const mpf_class &x, const mpf_class &y) {
+    
+}
